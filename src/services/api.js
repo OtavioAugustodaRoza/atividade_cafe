3e411e4385fc622f15518bbd91cee5e80a53db4a
Otávio <otavioaugustodaroza@gmail.com>
2026-09-01 13:57:24 -0300

    Update base URL for API configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "https://cafe-atividade-backend.onrender.com/api",
+ baseURL: "https://cafe-atividade-backend.onrender.com",
 });
 
 export default api;
